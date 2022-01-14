256903dcb1146ff954b871b1b555c41a55fa1fda
Dani <danixidev@gmail.com>
2022-01-14 13:49:53 +0100

    Add logging out

--- Employees/ViewController.swift
+++ Employees/ViewController.swift
@@ -8,16 +8,31 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        login(email: "danixidev@gmail.com", password: "T{hiz>#pl(")
+        //login(email: "danixidev@gmail.com", password: "T{hiz>#pl(")
+        logout(token: "$2y$10$rT0znvuWwS/6j/do5k/MI.6//Qy6k9ctlCfb7.3HCJyLSqjItH.Mq")
     }
     
     func login(email: String, password: String) {
         let parameters: Parameters = ["email": email, "password": password]
         
         AF.request("http://kurokiji.com/api/login", method: .put, parameters: parameters).responseDecodable(of: Body.self) { response in
-            
-            self.user = try? response.value?.user
-            print(response.value?.user)
+            if response.value?.status == 1 {
+                print(response.value?.user)
+                print("Succesful: \(response.value?.msg)")
+            } else {
+                print("Failed: \(response.value?.msg)")
+            }
+        }
+    }
+    func logout(token: String) {
+        let header: HTTPHeaders = ["token": token]
+        
+        AF.request("http://kurokiji.com/api/employee/logout", method: .put, headers: header).responseDecodable(of: Body.self) { response in
+            if(response.value?.status == 1) {
+                print("Succesful: \(response.value?.msg)")
+            } else {
+                print("Failed: \(response.value?.msg)")
+            }
         }
     }
 
@@ -27,7 +42,7 @@ class ViewController: UIViewController {
 struct Body: Codable {
     let status: Int
     let msg: String
-    let user: User
+    let user: User?
 }
 
 struct User: Codable {
