6e55e377fe50ddd9ecb13e917b8a1511534f805a
Dani <danixidev@gmail.com>
2022-01-14 12:28:02 +0100

    Add making petitions to insecure web pages

--- Employees/ViewController.swift
+++ Employees/ViewController.swift
@@ -8,7 +8,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        login(email: "", password: "")
+        login(email: "danixidev@gmail.com", password: "T{hiz>#pl(")
     }
     
     func login(email: String, password: String) {
@@ -17,8 +17,7 @@ class ViewController: UIViewController {
         AF.request("http://kurokiji.com/api/login", method: .put, parameters: parameters).responseDecodable(of: Body.self) { response in
             
             self.user = try? response.value?.user
-            
-            print(self.user!)
+            print(response.value?.user)
         }
     }
 
